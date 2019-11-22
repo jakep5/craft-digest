2e0305fa8cfa3b92525ce5cb9ab232aa04f0c409
jakep5 <jakepagel1@yahoo.com>
2019-11-22 10:51:36 -0600

    add recommend beer route and component

---
+++ src/routes/RecommendBeerPage/RecommendBeerPage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import RecommendedBeer from '../../components/RecommendedBeer/RecommendedBeer'
+import './RecommendBeerPage.css'
+
+export default class RecommendBeerPage extends Component {
+    render() {
+        return (
+            <>
+                <main role="main">
+                    <header role="banner">
+                        <h1 class="recommendBeerTitle">Here's a beer for you to try</h1>
+                    </header>
+
+                    <section class="recommendBeerSection">
+
+                        <div class="recommendedBeerHolder">
+                            <RecommendedBeer />
+                        </div>
+
+                    </section>
+                </main>
+            </>
+        )
+    }
+}
