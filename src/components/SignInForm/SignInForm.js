5ee777b6bd4cc15edb9764e35b542a5163ffc0d5
jakep5 <jakepagel1@yahoo.com>
2019-11-26 11:51:39 -0600

    finish login wiring

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import TokenServiceObject from '../../services/token-service'
+import AuthApiServiceObject from '../../services/auth-api-service'
 import './SignInForm.css'
 
 export default class SignInForm extends React.Component {
+
+    static defaultProps = {
+        onLoginSuccess: () => {} 
+    }
 
     state = {
         error: null
@@ -11,23 +16,34 @@ export default class SignInForm extends React.Component {
 
     handleSubmitAuthentication = e => {
         e.preventDefault();
+        this.setState({
+            error: null
+        });
+        
         const user_name = document.getElementById('signInUsername').value;
         const password = document.getElementById('signInPassword').value;
 
-        let token = TokenServiceObject.makeBasicAuthToken(user_name, password)
-
-        TokenServiceObject.saveAuthToken(token)
-
-        user_name = '';
-        password = '';
-        this.props.onLogInSuccess()
-
+        AuthApiServiceObject.logIn({
+            user_name: user_name,
+            password: password
+        })
+            .then(res => {
+                user_name = ''
+                password = ''
+                TokenServiceObject.saveAuthToken(res.authToken)
+                this.props.onLoginSuccess()
+            })
+            .catch(res => {
+                this.setState({
+                    error: res.error
+                })
+        })
     }
 
     render() {
         return (
             <>
-                <form id="signInForm">
+                <form id="signInForm" onSubmit={(e) => this.handleSubmitAuthentication(e)}>
                     <legend>Sign in</legend>
                     <br />
                     <label for="signInUsername">Username</label>
---
+++ src/services/auth-api-service.js
@@ -0,0 +1,20 @@
+import config from '../config'
+
+const AuthApiServiceObject = {
+    logIn(credentials) {
+        return fetch(`${config.API_ENDPOINT}/auth/login`, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(credentials)
+        })
+            .then(res => 
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+                )
+    }
+}
+
+export default AuthApiServiceObject
