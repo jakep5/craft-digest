fed8acaa626f15009cc2aca1b15e1b3b4917fb7e
jakep5 <jakepagel1@yahoo.com>
2019-11-26 13:35:39 -0600

    add beer post request

---
+++ src/services/beer-api-service.js
@@ -0,0 +1,39 @@
+import config from '../config'
+import TokenServiceObject from './token-service'
+
+const BeerApiServiceObject = {
+    getBeers(userId) {
+        return fetch(`${config.API_BASE_URL}/beers/${user_id}`, {
+            headers: {
+                'Authorization': `bearer ${TokenServiceObject.getAuthToken()}`
+            }
+        })
+    },
+
+    postBeer(beer) {
+        return fetch(`${config.API_BASE_URL}/beers`, {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json",
+                "Authorization": `bearer ${TokenService.getAuthToken()}`
+            },
+            body: JSON.stringify({
+                id: beer.id,
+                name: beer.name,
+                brewery_name: beer.brewery_name,
+                brewery_location: beer.brewery_location,
+                tasting_notes: beer.tasting_notes,
+                abv: beer.abv,
+                rating: beer.rating,
+                user_id: beer.user_id
+            })
+        })
+            .then(res =>
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()    
+                )
+    }
+}
+
+export default BeerApiServiceObject
