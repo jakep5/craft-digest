015c74dbf3e04a4023e2bfb314ce9290bc18b966
jakep5 <jakepagel1@yahoo.com>
2019-11-26 17:07:09 -0600

    code get user id function for get requests

--- src/components/BeerTriedList/BeerTriedList.js
+++ src/components/BeerTriedList/BeerTriedList.js
@@ -4,6 +4,7 @@ import { BeerConsumer } from '../../contexts/BeerContext'
 import { BeerContext } from '../../contexts/BeerContext'
 import './BeerTriedList.css'
 import AuthApiServiceObject from '../../services/auth-api-service'
+import BeerApiServiceObject from '../../services/beer-api-service'
 
 
 
@@ -12,10 +13,24 @@ export default class BeerTriedList extends React.Component {
     static contextType = BeerContext;
 
 
-    /* componentWillMount = () => {
-        const userId = //??????????????????????????
-        this.context.getBeers(userId)
-    } */
+    componentDidMount = () => {
+        const token = sessionStorage.getItem('craft-digest-token-key')
+        let payload = this.parseJwt(token)
+        let userId = payload.user_id
+        console.log(userId)
+/*         BeerApiServiceObject.getBeers(userId)
+ */
+    }
+
+    parseJwt = (token) => {
+        let base64Url = token.split('.')[1];
+        let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        return JSON.parse(jsonPayload)
+    }
 
     render() {
 
