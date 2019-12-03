b823ee497c0fbe93c84a8e6aa64366e771b3b3bf
jakep5 <jakepagel1@yahoo.com>
2019-12-03 11:24:29 -0600

    add mobile restriction for bubble chart

--- src/components/CommonTastingNotes/CommonTastingNotes.js
+++ src/components/CommonTastingNotes/CommonTastingNotes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BeerContext } from '../../contexts/BeerContext'
 import './CommonTastingNotes.css'
 import BubbleChart from '@weknow/react-bubble-chart-d3';
+import MediaQuery from 'react-responsive'
 
 export default class CommonTastingNotes extends Component {
 
@@ -47,43 +48,48 @@ export default class CommonTastingNotes extends Component {
 
         return (
 
-            <div id="word-cloud">
-                {Object.keys(data).length >= 3 ?
-                    <BubbleChart
-                    graph= {{
-                        zoom: .75,
-                        offsetX: -0.00,
-                        offsetY: -0.01,
-                    }}
-                    width={1000}
-                    height={800}
-                    padding={0} // optional value, number that set the padding between bubbles
-                    showLegend={true} // optional value, pass false to disable the legend.
-                    legendPercentage={20} // number that represent the % of with that legend going to use.
-                    legendFont={{
-                            family: 'Arial',
-                            size: 12,
-                            color: '#000',
-                            weight: 'bold',
-                        }}
-                    valueFont={{
-                            family: 'Arial',
-                            size: 12,
-                            color: '#fff',
-                            weight: 'bold',
-                        }}
-                    labelFont={{
-                            family: 'Arial',
-                            size: 16,
-                            color: '#fff',
-                            weight: 'bold',
-                        }}
-                    //Custom bubble/legend click functions such as searching using the label, redirecting to other page
-                    data={data}
-                />
+            <div id="bubbleChart">
+                    <MediaQuery minDeviceWidth={1000}>
+                        {Object.keys(data).length >= 3 ?
+                            <BubbleChart
+                            graph= {{
+                                zoom: .75,
+                                offsetX: -0.00,
+                                offsetY: -0.01,
+                            }}
+        /*                     width={1000}
+        */                    height={800}
+                            padding={0} // optional value, number that set the padding between bubbles
+                            showLegend={true} // optional value, pass false to disable the legend.
+                            legendPercentage={20} // number that represent the % of with that legend going to use.
+                            legendFont={{
+                                    family: 'Arial',
+                                    size: 12,
+                                    color: '#000',
+                                    weight: 'bold',
+                                }}
+                            valueFont={{
+                                    family: 'Arial',
+                                    size: 12,
+                                    color: '#fff',
+                                    weight: 'bold',
+                                }}
+                            labelFont={{
+                                    family: 'Arial',
+                                    size: 16,
+                                    color: '#fff',
+                                    weight: 'bold',
+                                }}
+                            //Custom bubble/legend click functions such as searching using the label, redirecting to other page
+                            data={data}
+                        />
 
-                : <p>You must have at least 3 distinct tasting notes to see data</p>
-                }
+                        : <p>You must have at least 3 distinct tasting notes to see data</p>
+                    }
+                </MediaQuery>   
+
+                <p id="warning">Bubble chart only available on desktop version</p>
+
             </div>
         )
     }
