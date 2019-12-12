78d6c9928fff0f607aba2a163b4771e8fdfcd6a8
jakep5 <jakepagel1@yahoo.com>
2019-12-12 16:45:04 -0600

    change base url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     API_KEY: process.env.REACT_APP_API_KEY,
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "https://warm-coast-56574.herokuapp.com",
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "https://craftdigest.herokuapp.com",
     TOKEN_KEY: 'craft-digest-token-key'
 }
